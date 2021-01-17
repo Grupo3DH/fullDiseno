1086a6bc7386d93fe6fc4b8547d92033a80e1ec1
melinaacosta <melii.a@hotmail.com>
2021-01-17 00:50:22 -0300

    quizá no sea el vino

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -1,19 +1,7 @@
 const express = require("express");
 const router = express.Router(); // guardo solo la funcionalidad de router de express
 const path = require("path");
-const multer  = require('multer'); // para subir avatars o fotos
 const indexController = require("../controllers/indexController");
-
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, '/tmp/my-uploads')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now())
-    }
-  })
-   
-  var upload = multer({ storage: storage })
 
 
 router.get("/", indexController.index)
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
-const { ckeck, validationResult, body, check} = require("express-validator");
 const userController = require("../controllers/userController");
+const registerValidation = require("./validations/registerValidations"); // exporto las validaciones
+const userController = require("../controllers/userController");
+const multerRegister = require("../middlewares/multerRegister"); // tengo que ponerle upload???
 
 
 // /user/register 
@@ -10,10 +12,7 @@ router.get("/register", userController.register);
 router.post("/register", upload.any(), userController.create);
 
 // /user/login
-router.get("/login", [
-    check("contraseña").isLength({min:8}).isEmpty.withMessage("Su contraseña debe contener al menos 8 caracteres"),  //preguntar si esto funciona asi...
-    check("correo electrónico").isEmail().isEmpty.withMessage("Su contraseña es inválida"),
-], userController.login)
+router.get("/login", registerValidation, userController.login) // faltan las validaciones
 
 
 module.exports = router;

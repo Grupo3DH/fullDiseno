55c36afe6a6fe3b508bf916e79db20fea1a9e254
melinaacosta <melii.a@hotmail.com>
2021-01-18 22:05:56 -0300

    quemada

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,8 +7,8 @@ userController = {
     },
     processLogin: function(req,res){
         let error = validationResult(req);
-        if (error.isEmpty()){
-            return res.render("login", {errors: {msg: "Error"}})
+        if (error.isEmpty()){   
+            return res.render("login", {errors: {msg: "Error"}}) // si errores esta vacio registro a la personas
         } else {
             return res.render("login", {errors: errors.errors})
         }

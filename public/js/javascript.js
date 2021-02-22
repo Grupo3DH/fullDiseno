463b5870b08ace661f1d1c1628975c8056ff9923
melinaacosta <melii.a@hotmail.com>
2021-02-22 02:05:07 -0300

    un poquito de js

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,11 +1,29 @@
-// window.addEventListener("click", function(){
+window.addEventListener("load", function(){
+    let search = document.querySelector(".icono-search")
+    search.addEventListener("click", function(e){
+        e.preventDefault()
+        let button =  document.querySelector(".button-search")
+        button.style.display = "inherit" 
+      })
+
+
+})
+
+
+// window.addEventListener("load", function(){
 //     let login = document.querySelector(".inicio")
 //     login.addEventListener("click", function(){
 //         window.alert("hola")
 //     })
 // })
 
+// window.addEventListener("load", function(){
+//     let productos = document.querySelector(".p")
+//     productos.addEventListener("mouseover", function(){
+//         style.color = "red"
+//       })
 
 
+// })
  
 

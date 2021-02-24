f840a4403a3276be2ddbc8c23a03130f8be94871
melinaacosta <melii.a@hotmail.com>
2021-02-24 00:49:02 -0300

    hice el slide show

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,13 +1,68 @@
 window.addEventListener("load", function(){
-    let search = document.querySelector(".icono-search")
+
+    var search = document.querySelector(".icono-search");
+    var button =  document.querySelector(".button-search");
+
+
     search.addEventListener("click", function(e){
         e.preventDefault()
-        let button =  document.querySelector(".button-search")
-        button.style.display = "inherit" 
-      })
+        button.style.display = "inherit"
+    })
 
 
-})
+    var slideIndex = 0;
+    var slides = document.querySelectorAll(".promo");
+    var intervaloslide = setInterval(showSlides, 3000);
+    function showSlides() {
+      
+      var i;
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+        slides[i].classList.add("active"); 
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {
+        slideIndex = 1}    
+      // for (i = 0; i < slides.length; i++) {
+      //   slides[i].className.replace(" active");
+      // }
+      slides[slideIndex-1].style.display = "block";  
+      slides[slideIndex-1].className += " active";
+      
+    }
+
+
+
+
+
+
+
+
+  })
+
+     
+  
+  
+  
+  
+  
+  
+  // let inputSearch = document.getElementById("search");
+
+      // inputSearch.addEventListener("submit",funtion(){
+      //   if(inputSearch.value != ""){
+      //   var form-search = document.querySelector(".formSearch")
+      //     form-search.submit()
+      //   }
+      //   })
+    
+  
+      // let newsletter = document.querySelector(".newsletterInput")
+      // newsletter.addEventListener("submit",funtion(){
+      //   newsletter.s
+      // })
+
+
 
 
 // window.addEventListener("load", function(){

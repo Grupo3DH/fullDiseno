2fa390a9a9102043d4d51476ee9704149f425584
melinaacosta <melii.a@hotmail.com>
2021-02-24 23:50:14 -0300

    subiendo a heroku

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,84 +1,80 @@
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
 
-    var search = document.querySelector(".icono-search");
-    var button =  document.querySelector(".button-search");
+  var search = document.querySelector(".icono-search");
+  var input = document.querySelector(".button-search input");
+  var formSearch = document.querySelector(".formSearch")
 
-
-    search.addEventListener("click", function(e){
-        e.preventDefault()
-        button.style.display = "inherit"
-    })
-
-
-    var slideIndex = 0;
-    var slides = document.querySelectorAll(".promo");
-    var intervaloslide = setInterval(showSlides, 3000);
-    function showSlides() {
-      
-      var i;
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; 
-        slides[i].classList.add("active"); 
-      }
-      slideIndex++;
-      if (slideIndex > slides.length) {
-        slideIndex = 1}    
-      // for (i = 0; i < slides.length; i++) {
-      //   slides[i].className.replace(" active");
-      // }
-      slides[slideIndex-1].style.display = "block";  
-      slides[slideIndex-1].className += " active";
-      
-    }
-
-
-
-
-
-
-
+  search.addEventListener("click", function (e) {
+    e.preventDefault();
+    input.style.display = "block"
 
   })
 
-     
-  
-  
-  
-  
-  
-  
+  input.addEventListener("focus", function () {
+    if (this.value == 'Buscar...') { this.value = "" }
+  })
+
+
+  // function buscar(){
+  //   if (input.value=="") {
+  //       alert("Que producto quiere buscar?");
+  //   }
+  //   else{
+  //       formSearch.submit(); 
+  //   }
+
+  // }
+
+  //SLIDES
+
+  var slideIndex = 0;
+  var slides = document.querySelectorAll(".promo");
+  var intervaloslide = setInterval(showSlides, 3000);
+  function showSlides() {
+    var i;
+    for (i = 0; i < slides.length; i++) {
+      slides[i].classList.add("ocultar");
+      slides[i].classList.add("active");
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1
+    }
+    // for (i = 0; i < slides.length; i++) {
+    //   slides[i].className.replace(" active");
+    // }
+    slides[slideIndex - 1].classList.remove("ocultar");
+    slides[slideIndex - 1].className += " active";
+
+  }
+
+//NEWSLWTTER
+  var newsletter = document.querySelector(".newsletterInput");
+
+  console.log(newsletter)
+  newsletter.addEventListener("focus", function () {
+    if (this.value == 'NEWSLETTER') { this.value = "" };
+    newsletter.style.letterSpacing = "inherit"
+
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
   // let inputSearch = document.getElementById("search");
 
-      // inputSearch.addEventListener("submit",funtion(){
-      //   if(inputSearch.value != ""){
-      //   var form-search = document.querySelector(".formSearch")
-      //     form-search.submit()
-      //   }
-      //   })
-    
-  
-      // let newsletter = document.querySelector(".newsletterInput")
-      // newsletter.addEventListener("submit",funtion(){
-      //   newsletter.s
-      // })
-
-
-
-
-// window.addEventListener("load", function(){
-//     let login = document.querySelector(".inicio")
-//     login.addEventListener("click", function(){
-//         window.alert("hola")
-//     })
-// })
-
-// window.addEventListener("load", function(){
-//     let productos = document.querySelector(".p")
-//     productos.addEventListener("mouseover", function(){
-//         style.color = "red"
-//       })
-
-
-// })
- 
-
+  //     inputSearch.addEventListener("submit",funtion(){
+  //       if(inputSearch.value != ""){
+  //       var form-search = document.querySelector(".formSearch")
+  //         form-search.submit()
+  //       }
+  //       })
+  // 

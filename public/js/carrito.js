ffe0874e1e94ff67d21731eddb4e79311574c798
melinaacosta <melii.a@hotmail.com>
2021-04-11 19:09:20 -0300

    carrito de compras

--- public/js/carrito.js
+++ public/js/carrito.js
@@ -1,30 +1,42 @@
 let agregar = document.getElementById('agregar');
 agregar.addEventListener('click',function(){
 
-    console.log("estoy apretando")
-
+    let urlArray =  window.location.href.split("/");
+    let pepe = urlArray[urlArray.length - 1];
     let imagenProducto = document.getElementById("imagen");
     let nombreProducto = document.getElementById('nombre').innerText;
     let precioProducto = document.getElementById('precio').innerText;
     let cantProducto = document.getElementById('count').value;
 
     let producto = {
+        idProducto: pepe,
         imagenProducto,
         nombreProducto,
         precioProducto,
         cantProducto
     }
 
+  
+
     if( localStorage.length == 0){
-        let carrito = []
+        let carrito = [];
         carrito.push(producto)  
         localStorage.setItem('carrito',JSON.stringify(carrito)) 
-        console.log(carrito)
+       
     } else {
         let carrito = JSON.parse(localStorage.carrito)
-        carrito.push(producto)  
-        localStorage.setItem('carrito',JSON.stringify(carrito)) 
-        console.log(carrito)
+        let  arrayProductos = carrito.filter(function(producto){    //veo si existe en el localstorage un producto con el id igual al mismo que acabo de agregar 
+           return producto.idProducto == pepe    //DEVUELVE UN booleano que me dice true si tiene ese id o false si no lo tiene
+        })
+        if(arrayProductos.length == 0){
+            carrito.push(producto)  
+            localStorage.setItem('carrito',JSON.stringify(carrito)) ;
+        } else {
+            arrayProductos[0].cantProducto = parseInt(arrayProductos[0].cantProducto) + 1;
+            localStorage.setItem('carrito',JSON.stringify(carrito)); 
+        }
+        
+        
     }    
- alert(nombreProducto+' agregado al carrito')
+        alert(nombreProducto+' agregado al carrito')
 })
---
+++ public/js/guardadoCarrito.js
@@ -0,0 +1,81 @@
+window.addEventListener("load",(e)=>{ 
+    if(typeof localStorage.carrito == "undefined" || typeof
+    localStorage.carrito == "[]" ){
+        let div = document.getElementById("guardadoCarrito")
+        div.innerHTML += "<h2> no hay productos en tu carrito </h2>"
+    } else {
+        let carrito =  JSON.parse(localStorage.carrito)
+        for(let i = 0; i < carrito.length; i++){
+           let producto = carrito[i]
+           let div = document.getElementById("guardadoCarrito")
+           
+            let contenido =  `
+
+            <section class="tablaProductos">
+        <div class="descProdCarrito articulo">
+               <img src="/img/sueterVerde-2.png" alt="">
+            <div class="nomColCarrito">
+                <h3> ${producto.nombreProducto} </h3>
+                <h3>Color: verde militar</h3>
+                    </div>
+                    </div>
+                <div>
+    
+<button onclick="contadormenos()">-</button>
+<input type="text" style="text-align: center; width: 30px;" value="1">
+<button onclick="contadormas()">+</button>
+ </div>
+                                        <h2 class="precio">$<%= array[i].precioProducto %>
+                                        </h2>
+                                        <i class="borrar fa fa-trash"></i></a>
+    </section> `
+                                    div.innerHTML += contenido;
+        }
+        
+
+    }
+
+
+    
+   
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function deleteAll() {
+localStorage.clear()
+    location.reload()
+    alert('Carrito vaciado')
+}
+function deleteItem(id) {
+let carrito = JSON.parse(localStorage.carrito)
+carrito = carrito.filter((producto, i) => {
+ return i !== id;
+});
+localStorage.setItem('carrito', JSON.stringify(carrito));
+console.log(carrito)
+    location.reload();
+     }
